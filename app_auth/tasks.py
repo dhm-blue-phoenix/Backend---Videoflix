0cabc2da08226eb6ab8d5d5dbfc5ea424f0a5d6e
dhm-blue-phoenix <blue.dhm.dev@gmail.com>
2026-04-20 21:08:49 +0200

    refactor: move email templates to global directory and update Django settings and task paths accordingly

--- app_auth/tasks.py
+++ app_auth/tasks.py
@@ -17,7 +17,7 @@ def send_email_activation_task(user_id):
     url = f"{settings.FRONTEND_URL}/pages/auth/activate.html?uid={uid}&token={token}"
     subject = "Aktiviere deinen Videoflix Account"
     ctx = {'url': url, 'frontend_url': settings.FRONTEND_URL, 'backend_url': settings.BACKEND_URL, 'name': name}
-    html_msg = render_to_string('app_auth/email_activation.html', ctx)
+    html_msg = render_to_string('email_activation.html', ctx)
     return send_mail(subject, "", settings.DEFAULT_FROM_EMAIL, [user.email], html_message=html_msg)
 
 def send_email_password_reset_task(user_id):
@@ -28,5 +28,5 @@ def send_email_password_reset_task(user_id):
     url = f"{settings.FRONTEND_URL}/pages/auth/confirm_password.html?uid={uid}&token={token}"
     subject = "Passwort zurücksetzen - Videoflix"
     ctx = {'url': url, 'frontend_url': settings.FRONTEND_URL, 'backend_url': settings.BACKEND_URL}
-    html_msg = render_to_string('app_auth/email_password_reset.html', ctx)
+    html_msg = render_to_string('email_password_reset.html', ctx)
     return send_mail(subject, "", settings.DEFAULT_FROM_EMAIL, [user.email], html_message=html_msg)
